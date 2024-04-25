b29a71351667954f935d2bbf17be853f960aa2ac
Kea Johnston <kjohnsto@berkeley.edu>
2024-04-25 16:25:44 -0500

    Working on pruning algorithm

--- transfer/transferscripts.py
+++ transfer/transferscripts.py
@@ -2,8 +2,8 @@ import shutil
 import os.path
 
 #this script is for transfering files from the ortery computer to the network drive.
-def transferToNetworkDirectory(jobname, inputdir, transferdir, prune=False):
-    print(f"inputdir is: {inputdir}")
+def transferToNetworkDirectory(jobname, filestocopy, transferdir, prune=False):
+    
     #transfer image files in directory to the networkdrive with a subfolder called jobname.
     if not os.path.exists(transferdir):
         print(f"Invalid network directory in config.json: either {transferdir} does not exist, or you do not have permission to use it.")
@@ -11,7 +11,16 @@ def transferToNetworkDirectory(jobname, inputdir, transferdir, prune=False):
     destpath = os.path.join(transferdir, jobname)
     if not os.path.exists(os.path.join(transferdir,jobname)):
         os.mkdir(destpath)
-    filestocopy = [f for f in os.listdir(inputdir) if f.endswith("cr2")]
-    for f in filestocopy:
-        shutil.copy(os.path.join(inputdir,f),destpath)
 
+    for f in filestocopy:
+        shutil.copy(f,destpath)
+
+def pruneOrteryPics(filestocopy, orteryconfig):
+    picsperrevolution=orteryconfig["pics_per_revolution"]
+    desirednumbers=orteryconfig["pics_per_cam"]
+    expectedfiles = len(desirednumbers)*picsperrevolution
+    filestocopy.sort()
+    if(len(filestocopy)!=expectedfiles):
+        print(f"Cannot prune: the expected number of files was {expectedfiles}, but there were really {len(filestocopy)} files in the folder.")
+        return filestocopy
+    print(filestocopy)
