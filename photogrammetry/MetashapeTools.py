1513ba5c12327e97d7f840e39cf073a5734d4ca5
Kea Johnston <kjohnston@berkeley.edu>
2024-05-10 14:18:45 -0500

    Script now builds an untextured model.

--- photogrammetry/MetashapeTools.py
+++ photogrammetry/MetashapeTools.py
@@ -1,47 +1,67 @@
 import os
 import Metashape
+import shutil
 
 def buildBasicModel(photodir, outputdir, projectname, config):
     """Builds a model using the photos in the directory specified."""
     #Open a new document
     projectpath = os.path.join(outputdir,projectname+".psx")
     doc = Metashape.Document()
-    if os.path.exists(projectpath):
-        doc.open(path=projectpath)
-    doc.save(path=projectpath)
-    #add  a new chunk
-    chunk=None
-    if len(doc.chunks)==0:
-        chunk = doc.addChunk()
-    else:
-        chunk=doc.chunks[0]
-    #add the photos in the specified directory to that chunk.
-    images = os.listdir(photodir)
-    for i in images:
-        if os.path.splitext(i)[1] in [".jpg", ".tiff",".tif"]:
-            chunk.addPhotos(os.path.join(photodir,i))
-    doc.save()
-    #add masks if they exist.
-    maskKeypoints=False
-    if config["mask_path"]:
-        mp = os.path.join(config["mask_path"])
-        ext = config["mask_ext"]
-        template = f"{mp}{os.sep}{{filename}}.{ext}"
-        chunk.generateMasks(template,Metashape.MaskingMode.MaskingModeFile)
-        maskKeypoints=True
-    chunk.matchPhotos(downscale=0,
-                      generic_preselection=True,
-                      reference_preselection=True,
-                      reference_preselection_mode=Metashape.ReferencePreselectionMode.ReferencePreselectionSource,
-                      filter_mask=maskKeypoints,
-                      mask_tiepoints=False,
-                      filter_stationary_points=True,
-                      tiepoint_limit=0,
-                      reset_matches=True
-    )
-    chunk.alignCameras()
-    doc.save()
-    refineSparseCloud(doc, chunk, config)
+    try:
+        if os.path.exists(projectpath):
+            doc.open(path=projectpath)
+        doc.save(path=projectpath)
+        #add  a new chunk
+        chunk=None
+        if len(doc.chunks)==0:
+            chunk = doc.addChunk()
+        else:
+            chunk=doc.chunks[0]
+        #add the photos in the specified directory to that chunk.
+        #don't add cameras if chunk already has them. If you want to build a new model, go delete the old one and rerun. 
+        #Only add masks if adding new photos (for now.)
+        if len(chunk.cameras)==0:
+            images = os.listdir(photodir)
+            for i in images:
+                if os.path.splitext(i)[1] in [".jpg", ".tiff",".tif"]:
+                    chunk.addPhotos(os.path.join(photodir,i))
+            doc.save()
+            #add masks if they exist.
+            maskKeypoints=False
+            if config["mask_path"]:
+                mp = os.path.join(config["mask_path"])
+                ext = config["mask_ext"]
+                template = f"{mp}{os.sep}{{filename}}.{ext}"
+                chunk.generateMasks(template,Metashape.MaskingMode.MaskingModeFile)
+                maskKeypoints=True
+            chunk.matchPhotos(downscale=config["sparse_cloud_quality"],
+                            generic_preselection=True,
+                            reference_preselection=True,
+                            reference_preselection_mode=Metashape.ReferencePreselectionMode.ReferencePreselectionSource,
+                            filter_mask=maskKeypoints,
+                            mask_tiepoints=False,
+                            filter_stationary_points=True,
+                            tiepoint_limit=0,
+                            reset_matches=False
+            )
+            chunk.alignCameras()
+        doc.save()
+        refineSparseCloud(doc, chunk, config)
+        #build model.
+        if not chunk.model:
+            chunk.buildDepthMaps(downscale=config["model_quality"], filter_mode = Metashape.FilterMode.MildFiltering)
+            chunk.buildModel(source_data = Metashape.DataSource.DepthMapsData)
+            doc.save()
+        
+    except Exception as e:
+        #maybe if metashape leaves a lock on the file, go nuke it in the .files subfolder.
+        #There doesn't seem to be a clean way to close
+        filesfolder = os.path.join(outputdir,projectname+".files")
+        if os.path.exists(os.path.join(filesfolder,"lock")):
+            os.remove(os.path.join(filesfolder,"lock"))          
+        #the document.
+        raise e
+
     
 
 #runs the optimize camera function, setting a handfull of the statistical fitting options to true only if the parameter is true,
@@ -58,7 +78,6 @@ def optimizeCameras(chunk, final_optimization=False):
                           fit_k4=final_optimization,
                           fit_p1 = True,
                           fit_p2=True,
-                          fit_p3=final_optimization,
                           fit_p3=final_optimization,
                           adaptive_fitting=False,
                           tiepoint_covariance=False)
@@ -82,11 +101,11 @@ def refineSparseCloud(doc,chunk,config):
     removeAboveErrorThreshold(chunk,
                             Metashape.TiePoints.Filter.ProjectionAccuracy,
                             error_thresholds["projection_accuracy"],
-                            error_thresholds["project_accuracy_max_selection"])
+                            error_thresholds["projection_accuracy_max_selection"])
     chunk.optimizeCameras()
     doc.save()
     #remove points with a reprojection error of above threshold, only removing a set percentage of overall points at a time.
-    num_points = len(chunk.tie_points)
+    num_points = len(chunk.tie_points.points)
     min_remaining_points = num_points-num_points*error_thresholds["reprojection_max_selection"]
     reachedgoal = False
     while num_points > min_remaining_points and not reachedgoal:
@@ -95,29 +114,31 @@ def refineSparseCloud(doc,chunk,config):
                                     error_thresholds["reprojection_error"],
                                     error_thresholds["reprojection_max_selection_per_iteration"])
         chunk.optimizeCameras()
-        num_points = len(chunk.tie_points)
+        num_points = len(chunk.tie_points.points)
     chunk.optimizeCameras(True)
-    doc.Save()
-            
+    doc.save()
+
     
 # attempts to select and remove all points above a given error threshold in max_error, up to a maximum percentage of acceptable points to remove, max_points.
 # returns true if it succeeds in removing all points with error higher than the threshold without first reaching the maximum selection.
 def removeAboveErrorThreshold(chunk, filtertype,max_error,max_points):
     removed_above_threshold=False
-    points = chunk.tie_points
-    num_points = len(points)
+    tiepoints = chunk.tie_points
+    num_points = len(tiepoints.points)
     max_removal = num_points*max_points
     print(f"Selecting and attempting to remove points with error {filtertype} above {max_error} with a max removal of {max_removal} of {num_points}.")
     errorfilter = Metashape.TiePoints.Filter()
-    errorfilter.init(points,filtertype)
-    errorvals = errorfilter.values.sort(reverse=True)
-    for i,v in errorvals.items():
+    errorfilter.init(tiepoints,filtertype)
+    errorvals = errorfilter.values.copy()
+    errorvals.sort(reverse=True)
+    for i,v in enumerate(errorvals):
         if v > max_error and  (i+1) <= max_removal:
             continue
         else:
             errorfilter.selectPoints(v)
-            points.removeSelectedPoints()
+            tiepoints.removeSelectedPoints()
             removed_above_threshold=(v<=max_error)
+            break
     return removed_above_threshold
 
 
