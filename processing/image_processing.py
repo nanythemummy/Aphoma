4915e3d4894eab1015a04e822f569b952310c1d5
Kea Johnston <kjohnsto@berkeley.edu>
2024-06-05 16:02:33 -0500

    added camera profiles and a function to grab exif data

--- processing/image_processing.py
+++ processing/image_processing.py
@@ -1,11 +1,15 @@
 import os
-import rawpy, subprocess, imageio
-import numpy
-from sys import platform
 from pathlib import Path
-from util import util
-import cv2
 import shutil
+import subprocess
+import imageio
+import rawpy
+import lensfunpy
+import cv2
+from PIL import Image as PILImage
+from PIL import ExifTags
+from util import util
+
 
 def buildMasks( imagefolder, outputpath, config):
     dropletpath = util.getConfigForPlatform(config["Masking_Droplet"])
@@ -30,17 +34,48 @@ def buildMasks( imagefolder, outputpath, config):
                 shutil.move(oldpath,os.path.join(maskdir,filename))
     shutil.rmtree(dropletoutput)
 
+def lensProfileCorrection(filepath,config):
+    clprofile = util.getCameraLensProfiles(config["camera"],config["lens"])
+    lensdb = lensfunpy.Database()
+    #both of these return a list, the first item of which should be our camera. If not, we need to be more specific.
+    cam = lensdb.find_cameras(clprofile["camera"]["maker"],clprofile["camera"]["model"])[0]
+    lens = lensdb.find_lenses(cam,clprofile["lens"]["maker"],clprofile["lens"]["model"])[0]
 
 def convertCR2toDNG(input,output,config):
     outputcmd = f"-d \"{output}/\""
     converterpath = util.getConfigForPlatform(config["DNG_Converter"])
     subprocess.run([converterpath,"-d",output,"-c", input], check = False)
 
+def getExifData(filename):
+    exif = {}
+    skiplist=["MakerNote","UserComment"] #These have a bunch of data that needs to be decoded somehow and I can't be stuffed to figure out how to do it.
+    with PILImage.open(filename) as pi:
+        exif = pi.getexif()
+        IFD_CODES = {i.value: i.name for i in ExifTags.IFD}
+        for code, val in exif.items():
+            if code in IFD_CODES:
+                propname = IFD_CODES[code]
+                print(propname)
+                ifd_data = exif.get_ifd(code)
+                for nk,nv in ifd_data.items():
+                    nested_tag = ExifTags.GPSTAGS.get(nk,None) or ExifTags.TAGS.get(nk,None) or nk
+                    print(nested_tag)
+                    if nested_tag in skiplist:
+                        continue
+                    exif[nested_tag]=nv
+            else:
+                tagname = ExifTags.TAGS.get(code,code)
+                exif[tagname]=val
+    
+
+    return exif           
+
+
 def convertCR2toTIF(input,output,config):
     fn = Path(input).stem
     with rawpy.imread(input) as raw:
         rgb = raw.postprocess(use_camera_wb=True)
-        imageio.imsave(path.join(output,fn+".tif"),rgb)
+        imageio.imsave(os.path.join(output,fn+".tif"),rgb)
 
 def convertToJPG(input, output):
     fn = Path(input).stem #get the filename.
--- util/util.py
+++ util/util.py
@@ -1,5 +1,5 @@
 from sys import platform
-
+import json
 
 def getConfigForPlatform(config):
     #In the json config, differing options for platform should folllow the format:
@@ -15,4 +15,15 @@ def getConfigForPlatform(config):
     else:
         return(config["Win"])
     
+def getCameraLensProfiles(cameraprofile,lensprofile):
+    setupinfo = {"lens":None,
+                 "camera":None}
+    profiles = {}
+    with open("CameraProfiles.json") as f:
+        profiles= json.load(f)
+    if cameraprofile in profiles["cameras"].keys():
+        setupinfo["camera"] = profiles["cameras"][cameraprofile]
+    if lensprofile in profiles["lenses"].keys():
+        setupinfo["lens"] = profiles["lenses"][lensprofile]
+    return setupinfo
 
